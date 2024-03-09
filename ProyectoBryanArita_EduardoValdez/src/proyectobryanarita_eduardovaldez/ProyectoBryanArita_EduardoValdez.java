8faf588dcd11c57f06d07363218aef514792dae1
evaldez26 <157846931+evaldez26@users.noreply.github.com>
2024-03-09 17:48:34 -0600

    Add files via upload

---
+++ ProyectoBryanArita_EduardoValdez/src/proyectobryanarita_eduardovaldez/ProyectoBryanArita_EduardoValdez.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package proyectobryanarita_eduardovaldez;
+
+/**
+ *
+ * @author guali
+ */
+public class ProyectoBryanArita_EduardoValdez {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Menu x = new Menu();
+        x.setVisible(true);
+    }
+    
+}
---
+++ ProyectoBryanArita_EduardoValdez/src/proyectobryanarita_eduardovaldez/palabras.java
@@ -0,0 +1,48 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package proyectobryanarita_eduardovaldez;
+
+/**
+ *
+ * @author guali
+ */
+public class palabras {
+    int posicion;
+    String palabra;
+
+    public palabras(int posicion) {
+        this.posicion = posicion;
+        String[] word = new String [16];
+        word[0] = "Azul".toUpperCase();
+        word[1] = "Carro".toUpperCase();
+        word[2] = "Error".toUpperCase();
+        word[3] = "Facil".toUpperCase();
+        word[4] = "Grave".toUpperCase();
+        word[5] = "fruto".toUpperCase();
+        word[6] = "Hielo".toUpperCase();
+        word[7] = "Ideas".toUpperCase();
+        word[8] = "Indio".toUpperCase();
+        word[9] = "Japon".toUpperCase();
+        word[10] = "Morir".toUpperCase();
+        word[11] = "Nacer".toUpperCase();
+        word[12] = "Perro".toUpperCase();
+        word[13] = "Reloj".toUpperCase();
+        word[14] = "Sabio".toUpperCase();
+        word[15] = "Tabla".toUpperCase();
+        
+        palabra = word[posicion];
+    }
+    
+    
+
+   
+    
+
+    
+    
+    
+            
+            
+}
